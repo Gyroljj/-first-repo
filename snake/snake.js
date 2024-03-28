bf9e339cc5b3ff92826f9b6a5fdde2a8fb5b324c
lijianjun <gyro239@outlook.com>
2024-03-28 21:03:56 +0800

    [ADD]snake.js添加

---
+++ snake/snake.js
@@ -0,0 +1,86 @@
+// 蛇对象
+class Snake{
+    constructor(select) {
+        this.map = document.querySelector(select);
+        // 蛇的运动方向
+        this.direction = "right";
+        // 蛇的数组(把蛇的头和身体都会存储到数组当中，头从数组的第0位开始)
+        this.snakeList = [];
+        //调用创建蛇方法
+        this.createSnake();
+        this.move();
+    }
+    // 创建蛇头的函数
+    createHead(){
+        // 获取数组当中的第0位 找到蛇头
+        const head = this.snakeList[0];
+
+        // 定义坐标
+        const pos = {x : 0, y : 0};
+
+        if (head){
+            // 如果有蛇头那么创建新的蛇头放到原先蛇头的后面坐标位置上
+            // 新蛇头坐标一定发生改变, 改变方向我们需要罗列一下
+            switch (this.direction) {
+                case "left":
+                    pos.x = head.offsetLeft - 20;
+                    pos.y = head.offsetTop;
+                    break;
+                case "right":
+                    pos.x = head.offsetLeft + 20;
+                    pos.y = head.offsetTop;
+                    break;
+                case "top":
+                    pos.x = head.offsetLeft;
+                    pos.y = head.offsetTop - 20;
+                    break;
+                case "bottom":
+                    pos.x = head.offsetLeft;
+                    pos.y = head.offsetTop + 20;
+                    break;
+
+                default:
+                    break;
+
+            }
+            // 把原先的蛇头变成身体
+            head.className = "body";
+
+        }
+
+
+
+
+        // 创建蛇头
+        const div = document.createElement("div");
+        // 定义 样式
+        div.className = "head";
+        // 把蛇头存入数组
+        this.snakeList.unshift(div);
+        // 给蛇头定义 坐标
+        div.style.left = pos.x + "px";
+        div.style.top = pos.y + "px"
+        // console.log(div);
+        // 放到地图当中
+        this.map.appendChild(div);
+    }
+    // 创建一条蛇
+    createSnake(){
+        for (let i = 0; i < 4; i++) {
+            this.createHead();
+        }
+    }
+    // 蛇移动的方法
+
+    move(){
+        /* 思路是把原先头部坐标后面增加一个蛇头，原本的蛇头变成身体，身体末尾的
+        的位置删除一个以此来实现视觉上的位移*/
+        // 从数组当中移除掉了
+        const body = this.snakeList.pop();
+        console.log(body);
+        // 从页面删除
+        body.remove();
+        // 新增蛇头
+        this.createHead();
+    }
+}
